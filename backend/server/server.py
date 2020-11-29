7686baad4b03764bb5b968b4d7fa16593d855da0
Kiran Gadhave <kirangadhave2@gmail.com>
2020-11-29 15:14:22 -0700

    feat: 🎸 Setup server

--- backend/server/server.py
+++ backend/server/server.py
@@ -1,22 +1,23 @@
-import os, sys
-sys.path.append('../inference-core')
 from flask import Flask
-from core import hello
-from routes.datasetRoutes import datasetRoute, listAllDatasets
-import yaml
+
+from ..inference_core.core import hello
+from .routes.datasetRoutes import datasetRoute, listAllDatasets
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.register_blueprint(datasetRoute)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return f"<div>{hello()}<div>"
 
-@app.route('/hello', methods=['GET'])
+
+@app.route("/hello", methods=["GET"])
 def helloWorld():
     return hello()
 
-if __name__ == "__main__":
+
+def start_server():
     listAllDatasets()
     app.run()
---
+++ run.py
@@ -0,0 +1,5 @@
+from backend.server.server import start_server
+
+if __name__ == "__main__":
+    print(__file__, __package__, __name__)
+    start_server()
