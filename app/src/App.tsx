551e69e420bedb0f1827c26fa0356b7212061f99
Kiran Gadhave <kirangadhave2@gmail.com>
2020-11-29 18:57:24 -0700

    feat: 🎸 Testing CORS

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Testing Edits!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [text, setText] = useState("What's Up?");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/hello")
+      .then((response) => response.text())
+      .then((data) => setText(data));
+  }, []);
+
+  return <div className="App">{text}</div>;
 }
 
 export default App;
