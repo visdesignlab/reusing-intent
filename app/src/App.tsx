36f98ab21d3a272858e98a94e8c3a357cb450840
Kiran Gadhave <kirangadhave2@gmail.com>
2020-12-04 16:15:26 -0700

    feat: 🎸 Testing brush components

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,16 +1,78 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import { extent, scaleLinear } from 'd3';
+import React, { FC, useCallback, useMemo, useState } from 'react';
 
-function App() {
-  const [text, setText] = useState("What's Up?");
+import cars from './cars';
+import MultiBrush, { Brush, Brushes, BrushOperation } from './components/Brush/Brush';
 
-  useEffect(() => {
-    fetch("http://127.0.0.1:5000/hello")
-      .then((response) => response.text())
-      .then((data) => setText(data));
+const xExtents = extent(cars.map((c) => c['Displacement']));
+const yExtents = extent(cars.map((c) => c['Weight_in_lbs']));
+
+const App: FC = () => {
+  const [brushes, setBrushes] = useState<Brushes>({});
+
+  const xScale = useMemo(() => {
+    const scale = scaleLinear().range([0, 800]).nice();
+
+    if (xExtents[0] !== undefined) scale.domain(xExtents);
+
+    return scale;
   }, []);
 
-  return <div className="App">{text}</div>;
-}
+  const yScale = useMemo(() => {
+    const scale = scaleLinear().range([800, 0]).nice();
+
+    if (yExtents[0] !== undefined) scale.domain(yExtents);
+
+    return scale;
+  }, []);
+
+  const brushUpdates = useCallback((affectedBrush: Brush, operation: BrushOperation) => {
+    const br = brushes;
+
+    switch (operation) {
+      case 'ADD':
+      case 'CHANGE':
+        br[affectedBrush.id] = affectedBrush;
+        console.log(`Brush ${operation}`);
+        break;
+      case 'REMOVE':
+        delete br[affectedBrush.id];
+        console.log('Brush Removed');
+        break;
+    }
+
+    setBrushes(br);
+  }, []);
+
+  return (
+    <div className="App" style={{ padding: '1em' }}>
+      <svg height="850" width="850">
+        <rect fill="none" height="850" stroke="black" width="850" />
+        <g transform="translate(25,25)">
+          {cars.map((car) => (
+            <circle
+              key={`${car['Name']} ${car['Year']} ${car['Weight_in_lbs']}`}
+              cx={xScale(car['Displacement'])}
+              cy={yScale(car['Weight_in_lbs'])}
+              fill="red"
+              opacity="0.7"
+              r="5"
+              stroke="black"
+              strokeWidth="0.5"
+            />
+          ))}
+          <MultiBrush
+            bottom={800}
+            brushes={brushes}
+            left={0}
+            right={800}
+            top={0}
+            update={brushUpdates}
+          />
+        </g>
+      </svg>
+    </div>
+  );
+};
 
 export default App;
