8320d82550f69e0dad4d896e5e48eb45213867d7
Kiran Gadhave <kirangadhave2@gmail.com>
2021-04-06 15:58:22 -0600

    fix: 🐛 img url issue

--- app/src/components/Landing/Landing.tsx
+++ app/src/components/Landing/Landing.tsx
@@ -1,6 +1,14 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
 import { observer } from 'mobx-react';
 import React from 'react';
-import { Card, CardActionArea, CardMedia, CardContent, Typography, CardActions, Button, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
@@ -14,41 +22,47 @@ const useStyles = makeStyles({
 });
 
 const Landing = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const demoList = ["Cluster Complex", "Cluster Simple", "Gapminder World", "Linear Regression Complex", "Linear Regression Simple", "Outliers"]
+  const demoList = [
+    'Cluster Complex',
+    'Cluster Simple',
+    'Gapminder World',
+    'Linear Regression Complex',
+    'Linear Regression Simple',
+    'Outliers',
+  ];
 
-    return (
+  return (
     <>
-        {demoList.map((curr) => {
-            return (
-              <Card key={curr} className={classes.root} variant="outlined">
-                <CardActionArea>
-                  <CardMedia
-                    className={classes.media}
-                    src="/public/landingPictures/clusterComplex.png"
-                    title="Cluster Complex Version 1"
-                />
-                  <CardContent>
-                    <Typography component="h2" variant="h5" gutterBottom>
-                      {curr}
-                    </Typography>
-                    <Typography color="textSecondary" component="p" variant="body2" />
-                  </CardContent>
-                </CardActionArea>
-                <CardActions>
-                  <Button color="primary" size="small">
-                    View Dataset
-                  </Button>
-                  <Button color="primary" size="small">
-                    View Notebook
-                  </Button>
-                </CardActions>
-              </Card>
-            );
-        })}
+      {demoList.map((curr) => {
+        return (
+          <Card key={curr} className={classes.root} variant="outlined">
+            <CardContent>
+              <CardMedia
+                className={classes.media}
+                image={`${process.env.PUBLIC_URL}/landingPictures/clusterComplex.png`}
+                title="Cluster Complex Version 1"
+              />
+            </CardContent>
+            <CardContent>
+              <Typography component="h2" variant="h5" gutterBottom>
+                {curr}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button color="primary" size="small">
+                View Dataset
+              </Button>
+              <Button color="primary" size="small">
+                View Notebook
+              </Button>
+            </CardActions>
+          </Card>
+        );
+      })}
     </>
-    );
+  );
 };
 
 export default observer(Landing);
