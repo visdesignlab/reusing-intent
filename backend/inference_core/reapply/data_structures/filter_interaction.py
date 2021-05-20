dc4cf0c6ad911f8aae65ee98bde743f920dabac9
Kiran Gadhave <kirangadhave2@gmail.com>
2021-05-20 12:05:47 -0600

    chore: cleanup for refactor

--- backend/inference_core/reapply/data_structures/filter_interaction.py
+++
@@ -1,9 +0,0 @@
-from backend.inference_core.reapply.data_structures.base_interaction import (
-    BaseInteraction,
-)
-
-
-class FilterInteraction(BaseInteraction):
-    def __init__(self, type, filterType, **kwargs):
-        super().__init__(type)
-        self.filterType = filterType
--- backend/server/database/schemas/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-
-# from .algorithms.cluster import *
-# from .algorithms.outlier import *
-from .datasetMetadata import *
-from .datasetRecord import *
-from .project import *
