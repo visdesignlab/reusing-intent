8c221d6d79755e39b5d437b51679b93684cf2432
Kiran Gadhave <kirangadhave2@gmail.com>
2021-05-23 19:24:16 -0600

    feat: 🎸 Added library bootstrap

---
+++ reapply-workflows/src/reapply_workflows/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.2.0'
+
+def hello():
+  return "HEllo, World!"
+
+def hello2():
+  return "Jello"
---
+++ reapply-workflows/tests/test_reapply_workflows.py
@@ -0,0 +1,5 @@
+from reapply_workflows import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
